fa9bcf1863004a51f1d708a4b1a260e77255e5b7
Kaka Ruto <borecollins@gmail.com>
2021-08-31 23:45:33 +0300

    Update to Rails 6.1.4.1

--- app/graphql/graphql_on_rails_schema.rb
+++ app/graphql/graphql_on_rails_schema.rb
@@ -4,13 +4,6 @@ class GraphqlOnRailsSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
 
-  # Opt in to the new runtime (default in future graphql-ruby versions)
-  use GraphQL::Execution::Interpreter
-  use GraphQL::Analysis::AST
-  # Add built-in connections for pagination
-  use GraphQL::Pagination::Connections
-  use GraphQL::Execution::Errors
-  # Turn off introspection entry points __schema && __type
   disable_introspection_entry_points if Rails.env.production?
 
   rescue_from(ActiveRecord::RecordNotFound) do |err, obj, args, ctx, field|
